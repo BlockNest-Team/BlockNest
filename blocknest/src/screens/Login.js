c44c13120db372687affc4a86803107a586e9f9e
ali-wattoo <m.ali@forenax.com>
2023-04-10 16:46:51 +0500

    css code is turn into resuable

--- blocknest/src/screens/Login.js
+++ blocknest/src/screens/Login.js
@@ -3,17 +3,17 @@ import '.././../src/index.scss'
 
 const Login = () => {
   return (
-    <div className="container ">
-      <div className="login-container">
+    <div className="container d-flex-justify-center">
+      <div className="login-container ">
         <div className="app-name">
           <h1>BlockNest</h1>
         </div>
         <div className="card ">
-          <div className="login-content">
+          <div className="login-content d-flex-align-center d-flex-col">
             <div className="login-heading ">
               <h1>Login</h1>
             </div>
-            <div className="profile-details">
+            <div className="profile-details d-flex-center d-flex-col">
               <div className="profile-pic">
                 <img src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-pic" />
               </div>
@@ -23,7 +23,9 @@ const Login = () => {
               </div>
 
               <div className="btn-container">
-                <a href='/' className='btn login-btn'>Authorize Login</a>
+                <a href='/' className=' login-btn'>
+                  <button className='btn'>Authorize Login</button>
+                </a>
               </div>
               <div className="redirect">
                 <a href='/'>Don’t have an account? Register</a>
