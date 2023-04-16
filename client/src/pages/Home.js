886f7558b64c02f19dbef176f3e89bfe69adbd0d
ali-wattoo <m.ali@forenax.com>
2023-04-17 00:42:44 +0500

    transaction history is added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import Wallet from '../components/wallet'
 import PostUpload from '../components/postUpload'
 import Post from '../components/post'
 import Status from '../components/status'
+import TransactionHistory from '../components/transactionHistory'
 
 import '../styles/pages/home.scss'
 
@@ -11,8 +12,9 @@ const home = () => {
     <div className="page-wrapper">
       <div className="container d-flex-justify-between">
 
-        <div className="wallet-container">
+        <div className="wallet-container d-flex-col">
           <Wallet />
+          <TransactionHistory />
         </div>
         <div className="feed-container">
           <PostUpload />
