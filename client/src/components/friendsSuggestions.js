e371a57322357705491fc3484e545e71e3ad9527
aliwatto <m.ali@forenax.com>
2023-04-18 17:32:13 +0500

    css in the login changed and comes from the theme

--- client/src/components/friendsSuggestions.js
+++ client/src/components/friendsSuggestions.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import '../styles/components/friendsSuggestions.scss'
+// import friendSuggestionsData from '../data/friendSuggestions.json'
 const FriendsSuggestions = () => {
   return (
-    <div>friendsSuggestions</div>
+    <div className="card">
+          <div className="friends-suggestions-container">
+            <div className="friends-suggestions d-flex-center">
+                  <div className="profile-pic ">
+                    <img
+                      src="https://www.w3schools.com/howto/img_avatar.png"
+                      alt="profile-pic"
+                    />
+                  </div>
+
+                  <div className="profile-name">
+                    <p>John Doe</p>
+                  </div>
+                </div>
+          </div>
+          </div>
   )
 }
 
