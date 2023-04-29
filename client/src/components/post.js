863a117967350f4058292388013079b274ab1c85
ali-wattoo <m.ali@forenax.com>
2023-04-29 14:38:24 +0500

    comment section is added

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -1,20 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/components/post.scss";
+import Comments from "../components/comment";
 import testPic from "../assets/pictures/test.png";
 import threeDotIcon from "../assets/svgs/three-dot.svg";
 import likeIcon from "../assets/svgs/like.svg";
 import likedIcon from "../assets/svgs/liked.svg";
 import commentIcon from "../assets/svgs/comment.svg";
 import shareIcon from "../assets/svgs/share.svg";
+import commentsData from '../data/commentData.json'
+import sendIcon from '../assets/svgs/send-btn.svg'
 const Post = () => {
 
   const [liked, setLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(12);
+  const [comments, setComments] = useState([]);
+  const [commentInput, setCommentInput] = useState('');
+  const [showComments, setShowComments] = useState(false);
+  const [expandedComments, setExpandedComments] = useState([]);
+  const [commentCount, setCommentCount] = useState(140);
+
+
+  useEffect(() => {
+    setComments(commentsData);
+  }, []);
 
   const handleLikeClick = () => {
     setLiked(!liked);
     setLikeCount(liked ? likeCount - 1 : likeCount + 1);
   };
+
+  const handleAddComment = (e) => {
+    e.preventDefault();
+    setComments([
+      {
+        userPic: "https://www.w3schools.com/howto/img_avatar.png",
+        userName: "Jhon Doe",
+        text: commentInput,
+      },
+      ...comments,
+    ]);
+    setCommentCount(commentCount + 1); // Add this line
+    setCommentInput("");
+  };
+
+
+  const toggleExpandedComment = (index) => {
+    const newExpandedComments = [...expandedComments];
+    if (newExpandedComments.includes(index)) {
+      newExpandedComments.splice(newExpandedComments.indexOf(index), 1);
+    } else {
+      newExpandedComments.push(index);
+    }
+    setExpandedComments(newExpandedComments);
+  };
+
 
   const formatCount = (count) => {
     if (count >= 1000000) {
@@ -65,30 +104,60 @@ const Post = () => {
             </div>
             <div className="groups d-flex-align-center ">
               <div className="stats-item comments">
-                <p>140</p>
+                <p>{formatCount(commentCount)}</p> {/* Update this line */}
                 <img src={commentIcon} alt="comment" />
               </div>
               <div className="stats-item">
-                <p>40</p>
+                <p>100</p> {/* Update this line */}
                 <img src={shareIcon} alt="share" />
               </div>
             </div>
           </div>
+
           <div className="post-actions d-flex-justify-between">
-            <div className="action-item" onClick={handleLikeClick}>
+            <div className="action-item d-flex-center" onClick={handleLikeClick}>
               <img src={liked ? likedIcon : likeIcon} alt="like" onClick={handleLikeClick} className={liked ? 'liked' : ''} />
               <p>Like</p>
             </div>
-            <div className="action-item">
+            <div className="action-item d-flex-center" onClick={() => setShowComments(!showComments)} >
               <img src={commentIcon} alt="comment" />
               <p>Comment</p>
             </div>
-            <div className="action-item">
+            <div className="action-item d-flex-center">
               <img src={shareIcon} alt="share" />
               <p>Share</p>
             </div>
           </div>
         </div>
+        {showComments && (
+          <div className="comment-section">
+            <form onSubmit={handleAddComment} className="comment-form">
+              <input
+                type="text"
+                value={commentInput}
+                onChange={(e) => setCommentInput(e.target.value)}
+                placeholder="Write a comment..."
+              />
+              <span className="add-comment-btn">
+                <img src={sendIcon} alt="add comment" onClick={handleAddComment} />
+              </span>
+            </form>
+
+            <ul>
+              {comments.map((comment, index) => (
+                <Comments
+                  key={index}
+                  userPic={comment.userPic}
+                  userName={comment.userName}
+                  text={comment.text}
+                  isExpanded={expandedComments.includes(index)}
+                  toggleExpanded={() => toggleExpandedComment(index)}
+                />
+              ))}
+            </ul>
+
+          </div>
+        )}
       </div>
     </div>
   );
