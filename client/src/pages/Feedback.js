231250c63ffee21fc50caca96d2b73b84850f1d8
ali-wattoo <m.ali@forenax.com>
2023-05-14 23:22:14 +0500

    feedback page is added

--- client/src/pages/Feedback.js
+++ client/src/pages/Feedback.js
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import '../styles/pages/feedback.scss'
+import NavBar from '../components/navbar';
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+    <>
+      <NavBar />
+      <div className="page-wrapper">
+        <div className="card privacy-and-security">
+          <div className="heading">
+            <h1>Feedback</h1>
+          </div>
+          <form>
+            <div className="formgroup">
+              <label htmlFor="issue">Your Issue</label>
+              <textarea
+                type="text"
+                name="issue"
+                id="issue"
+                rows={5}
+                required
+              />
+            </div>
+
+            <div className="formgroup">
+              <label htmlFor="fileUpload">
+                <p>Supporting Evidence</p>
+                <input
+                  type="file"
+                  name="fileUpload"
+                  id="fileUpload"
+                  required
+                  accept="image/png, image/jpg, image/gif, image/jpeg"
+                />
+              </label>
+            </div>
+            <div className='d-flex-center send-btn'>
+              <input
+                className="btn"
+                type="submit"
+                value="Send"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
