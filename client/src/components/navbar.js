b44d3a1e2d2da7727411baeb631875d5457f69e8
ali-wattoo <m.ali@forenax.com>
2023-05-14 16:41:41 +0500

    search bar is added in the navigation

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,6 +4,7 @@
 import { useEffect, useState } from "react";
 import "../styles/components/navbar.scss";
 import Spinner from "./spinner"
+import SearchBar from './searchbar'
 // import { TransactionContext } from "../context/context";
 
 const Navbar = () => {
@@ -108,7 +109,7 @@ const Navbar = () => {
       <div className="navbar__logo">BlockNest</div>
       <ul className={`navbar__links ${mobileMenuOpen ? "open" : ""}`}>
         <li className="navbar__item">
-          <a href="/" className="navbar__link">
+          <a href="/home" className="navbar__link">
             Home
           </a>
         </li>
@@ -131,6 +132,7 @@ const Navbar = () => {
       {/* <button className="navbar__connect-btn" onClick={connectToMetaMask}>
         Connect Wallet
       </button> */}
+      <SearchBar />
       <button className="navbar__connect-btn" onClick={connectWallet}>
         <span className="is-link has-text-weight-bold">
           {connecting
@@ -261,6 +263,137 @@ export default Navbar;
 //               6
 //             )}...${walletAddress.substring(38)}`
 //             : "Connect Wallet"}
+//         </span>
+//       </button>
+//       <div
+//         className={`navbar__hamburger ${mobileMenuOpen ? "open" : ""}`}
+//         onClick={toggleMobileMenu}
+//       >
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+// test 2 (this code is updated with wallet connect animation)
+
+// import { useEffect, useState } from "react";
+// import "../styles/components/navbar.scss";
+// import Spinner from "./spinner"
+// const Navbar = () => {
+//   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+//   const [connecting, setConnecting] = useState(false);  // New state
+
+
+//   const toggleMobileMenu = () => {
+//     setMobileMenuOpen(!mobileMenuOpen);
+//   };
+
+
+//   const [walletAddress, setWalletAddress] = useState("");
+
+//   useEffect(() => {
+//     getCurrentWalletConnected();
+//     addWalletListener();
+//   }, [walletAddress]);
+
+//   const connectWallet = async () => {
+//     setConnecting(true);  // Begin connecting
+//     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+//       try {
+//         const accounts = await window.ethereum.request({
+//           method: "eth_requestAccounts",
+//         });
+//         setWalletAddress(accounts[0]);
+//         console.log(accounts[0]);
+//       } catch (err) {
+//         console.error(err.message);
+//       }
+//     } else {
+//       console.log("Please install MetaMask");
+//     }
+//   };
+
+//   const getCurrentWalletConnected = async () => {
+//     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+//       try {
+//         const accounts = await window.ethereum.request({
+//           method: "eth_accounts",
+//         });
+//         if (accounts.length > 0) {
+//           setWalletAddress(accounts[0]);
+//           console.log(accounts[0]);
+//         } else {
+//           console.log("Connect to MetaMask using the Connect button");
+//         }
+//       } catch (err) {
+//         console.error(err.message);
+//       }
+//     } else {
+//       console.log("Please install MetaMask");
+//     }
+//     setConnecting(false);
+//   };
+
+//   const addWalletListener = async () => {
+//     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+//       window.ethereum.on("accountsChanged", (accounts) => {
+//         setWalletAddress(accounts[0]);
+//         console.log(accounts[0]);
+//       });
+//     } else {
+//       /* MetaMask is not installed */
+//       setWalletAddress("");
+//       console.log("Please install MetaMask");
+//     }
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar__logo">BlockNest</div>
+//       <ul className={`navbar__links ${mobileMenuOpen ? "open" : ""}`}>
+//         <li className="navbar__item">
+//           <a href="/home" className="navbar__link">
+//             Home
+//           </a>
+//         </li>
+//         <li className="navbar__item">
+//           <a href="/" className="navbar__link">
+//             Wallet
+//           </a>
+//         </li>
+//         <li className="navbar__item">
+//           <a href="/" className="navbar__link">
+//             Stories
+//           </a>
+//         </li>
+//         <li className="navbar__item">
+//           <a href="/" className="navbar__link">
+//             Notifications
+//           </a>
+//         </li>
+//         <li className="navbar__item">
+//           <a href="/" className="navbar__link">
+//             Settings
+//           </a>
+//         </li>
+//       </ul>
+//       <button className="navbar__connect-btn" onClick={connectWallet}>
+//         <span className="is-link has-text-weight-bold">
+//           {connecting
+//             ? <span className="is-link has-text-weight-bold">
+//               <Spinner />
+//             </span>
+//             : <span className="is-link has-text-weight-bold">
+//               {walletAddress && walletAddress.length > 0
+//                 ? `Connected: ${walletAddress.substring(0, 6)}...${walletAddress.substring(38)}`
+//                 : "Connect Wallet"}
+//             </span>}
 //         </span>
 //       </button>
 //       <div
