b35830615e28d2f78ecd2a03433efc2c36fec0c5
ali-wattoo <110724521+ali-wattoo@users.noreply.github.com>
2023-07-03 20:13:20 +0500

    transaction hsitory is fetching automatically now

--- client/src/pages/ViewWallet.js
+++ client/src/pages/ViewWallet.js
@@ -20,6 +20,50 @@ const ViewWallet = () => {
   const [requests, setRequests] = useState({ 1: [0], 0: [] });
 
   // fetch wallet  address
+  // useEffect(() => {
+  //   const fetchAccountData = async () => {
+  //     try {
+  //       const web3 = await getWeb3();
+  //       const accounts = await web3.eth.getAccounts();
+  //       const userAddress = accounts[0];
+  //       const userBalance = await web3.eth.getBalance(userAddress);
+  //       setAddress(userAddress).then(getNameAndBalance());
+  //       setBalance(web3.utils.fromWei(userBalance, "ether"));
+  //     } catch (error) {
+  //       console.error("Error fetching account data:", error.message);
+  //     }
+  //   };
+
+  //   fetchAccountData();
+  // }, []);
+
+  // fetbalance name route
+
+  // async function getNameAndBalance() {
+  //   console.log("moralis address " + address);
+  //   console.log("no. of request", requests);
+  //   const res = await axios.get(`http://localhost:8800/getNameAndBalance`, {
+  //     params: { userAddress: address },
+  //   });
+
+  //   const response = res.data;
+  //   console.log(response);
+
+  //   if (response.name[1]) {
+  //     setName(response.name[0]);
+  //   }
+  //   setBalance(String(response.balance));
+  //   // setDollars(String(response.dollars));
+  //   setHistory(response.history);
+  //   setRequests(response.requests);
+  // }
+
+  // useEffect(() => {
+  //   if (address !== "") {
+  //     getNameAndBalance();
+  //   }
+  // }, []);
+
   useEffect(() => {
     const fetchAccountData = async () => {
       try {
@@ -27,7 +71,7 @@ const ViewWallet = () => {
         const accounts = await web3.eth.getAccounts();
         const userAddress = accounts[0];
         const userBalance = await web3.eth.getBalance(userAddress);
-        setAddress(userAddress).then(getNameAndBalance());
+        setAddress(userAddress); // Removed .then(getNameAndBalance())
         setBalance(web3.utils.fromWei(userBalance, "ether"));
       } catch (error) {
         console.error("Error fetching account data:", error.message);
@@ -36,8 +80,6 @@ const ViewWallet = () => {
 
     fetchAccountData();
   }, []);
-
-  // fetbalance name route
 
   async function getNameAndBalance() {
     console.log("moralis address " + address);
@@ -62,14 +104,16 @@ const ViewWallet = () => {
     if (address !== "") {
       getNameAndBalance();
     }
-  }, []);
+  }, [address]); // <--- This is the dependency array
+
+
 
   return (
     <>
       <Navbar />
-      <button className="btn d-flex-center" onClick={getNameAndBalance}>
+      {/* <button className="btn d-flex-center" onClick={getNameAndBalance}>
         aa
-      </button>
+      </button> */}
       <div className="page-wrapper">
         <div className="grid-container">
           <div className="grid-left">
