fc049025bf8f194ab12febaa34f79a07e304cf8d
saim-jamil1 <saimjamil11@gmail.com>
2023-07-03 15:39:03 +0500

    following user updated

--- client/src/components/profileCard.js
+++ client/src/components/profileCard.js
@@ -2,18 +2,42 @@ import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import testPic from "../assets/pictures/test.png";
 import "../styles/components/profileCard.scss";
+import axios from "axios";
 
 const ProfileCard = ({ userProfile }) => {
   const { user } = useContext(AuthContext);
-  const [isFollowing, setIsFollowing] = useState(user?.followings.includes(userProfile._id));
+  const [isFollowing, setIsFollowing] = useState(
+    user?.followings.includes(userProfile._id)
+  );
 
-  const handleFollow = () => {
-    if (!isFollowing) {
-      console.log(`Following user with id ${userProfile._id}`);
-    } else {
-      console.log(`Unfollowing user with id ${userProfile._id}`);
+  const handleFollow = async () => {
+    try {
+      if (!isFollowing) {
+        await axios
+          .put(`/users/${userProfile._id}/follow`, { userId: user._id })
+          .then((res) => {
+            console.log(res.data);
+          });
+
+        console.log(`Following user with id ${userProfile._id}`);
+      } else {
+        // unfollow
+        await axios
+          .put(`/users/${userProfile._id}/unfollow`, { userId: user._id })
+          .then((res) => {
+            console.log(res.data);
+          });
+        console.log(`Unfollowing user with id ${userProfile._id}`);
+      }
+    } catch (err) {
+      console.log(err);
     }
+
     setIsFollowing(!isFollowing);
+
+    // if user profile is in the followings list of the current user then set button text to unfollow otherwiseto follow when the page loads
+    // when the button is clicked, if the button text is unfollow then send a request to the server to unfollow the user and set the button text to follow
+    // if the button text is follow then send a request to the server to follow the user and set the button text to unfollow
   };
 
   return (
