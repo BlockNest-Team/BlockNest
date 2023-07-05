e0020fe0fc660ff3fc84f386001d7a129223181c
ali-wattoo <110724521+ali-wattoo@users.noreply.github.com>
2023-07-05 16:04:47 +0500

    comments, regsiter are working with picture too

--- api/index.js
+++ api/index.js
@@ -409,6 +409,7 @@ mongoose.connect(
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
+app.use(express.json({ limit: "50mb" }));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
